625de9a86d31bf8f1f68e10f4f015c9ad4f54429
Paul Reiners <paul.reiners@gmail.com>
2016-04-27 15:29:12 -0500

    Predictions now being made on test set.

--- src/create_submission.py
+++ src/create_submission.py
@@ -15,11 +15,18 @@ from sklearn.cross_validation import train_test_split
 from util import log_loss 
 from k_neighbors_predictor import KNeighborsPredictor
 
-def get_and_clean_data():
+BEST_SCORE = 20.61577
+
+def get_data(file_path, tag):
     dtype = {'Name': str}
-    data = pd.read_csv('../data/train.csv', dtype=dtype, parse_dates=['DateTime'], index_col=0)
-    data = data.drop(['OutcomeSubtype', 'Name', 'DateTime'], axis=1)
-    data = data.dropna()
+    data = pd.read_csv(file_path, dtype=dtype, parse_dates=['DateTime'], index_col=0)
+    data['tag'] = tag
+    
+    return data
+
+def clean_data(data, is_test=False):
+    drop_cols = ['OutcomeSubtype', 'Name', 'DateTime']
+    data = data.drop(drop_cols, axis=1)
     categorical_columns = ["OutcomeType", "AnimalType", "SexuponOutcome", "Breed", "Color"]
     for categorical_column in categorical_columns:
         data[categorical_column] = data[categorical_column].astype('category')
@@ -47,9 +54,21 @@ def convert_age_to_days(age_str):
         return np.nan
 
 if __name__ == '__main__':
-    data = get_and_clean_data()
-    X = data.drop(['OutcomeType'], axis=1)
-    y = data['OutcomeType']
+    train_data = get_data('../data/train.csv', 'train')
+    test_data = get_data('../data/test.csv', 'test')
+    print test_data.info()
+    all_data = train_data.append(test_data)
+    all_data = clean_data(all_data)
+    
+    train_data = all_data[all_data['tag'] == 'train']
+    train_data = train_data.drop(['tag'], axis=1)
+    train_data = train_data.dropna()
+    test_data = all_data[all_data['tag'] == 'test']
+    test_data = test_data.drop(['OutcomeType', 'tag'], axis=1)
+    test_data = test_data.fillna(test_data.mean())
+    
+    X = train_data.drop(['OutcomeType'], axis=1)
+    y = train_data['OutcomeType']
     X_train, X_test, y_train, y_test = train_test_split(X, y)
 
     predictor = KNeighborsPredictor()
@@ -59,3 +78,7 @@ if __name__ == '__main__':
     possible_outcomes = ['Adoption', 'Died', 'Euthanasia', 'Return_to_owner', 'Transfer']
     ll = log_loss(y_test, 'OutcomeType', predictions_df, possible_outcomes)
     print "%.5f" % ll
+    
+    if ll < BEST_SCORE:
+        test_predictions = predictor.predict(test_data)
+        print test_predictions

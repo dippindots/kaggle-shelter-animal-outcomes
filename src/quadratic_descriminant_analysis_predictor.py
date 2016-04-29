e202cea9c9e3f7e4881b83f8cfac5c083c1a38e1
Paul Reiners <paul.reiners@gmail.com>
2016-04-28 19:11:29 -0500

    Quadratic Discriminant Analysis

---
+++ src/quadratic_descriminant_analysis_predictor.py
@@ -0,0 +1,29 @@
+'''
+Created on Apr 27, 2016
+
+MyLLScore:     26.94625
+KaggleLLScore:
+
+@author: Paul Reiners
+'''
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+from predictor_base import PredictorBase
+
+
+class QuadraticDiscriminantAnalysisPredictor(PredictorBase):
+    '''
+    Quadratic Discriminant Analysis
+    '''
+
+    def __init__(self):
+        self.clf = QuadraticDiscriminantAnalysis()
+
+    def fit(self, X_train, y_train):
+        self.clf.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        predictions = self.clf.predict_proba(X_test)
+        predictions_df = self.bundle_predictions(predictions)
+
+        return predictions_df

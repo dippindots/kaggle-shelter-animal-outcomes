6db5a3fb7bda12684696bdafcea340205763c220
Paul Reiners <paul.reiners@gmail.com>
2016-05-01 16:23:34 -0500

    Optimizing params.

--- src/classifiers/linear_descriminant_analysis_predictor.py
+++ src/classifiers/linear_descriminant_analysis_predictor.py
@@ -6,9 +6,11 @@ KaggleLLScore:
 
 @author: Paul Reiners
 '''
+from sklearn import grid_search
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 from classifiers.predictor_base import PredictorBase
+from util import get_data, preprocess_data
 
 
 class LinearDiscriminantAnalysisPredictor(PredictorBase):
@@ -16,7 +18,8 @@ class LinearDiscriminantAnalysisPredictor(PredictorBase):
     Linear Discriminant Analysis
     '''
 
-    def __init__(self):
+    def __init__(self, animal_type):
+        self.animal_type = animal_type
         self.clf = LinearDiscriminantAnalysis()
 
     def fit(self, X_train, y_train):
@@ -27,3 +30,25 @@ class LinearDiscriminantAnalysisPredictor(PredictorBase):
         predictions_df = self.bundle_predictions(predictions)
 
         return predictions_df
+
+    def find_best_params(self):
+        parameters = {'solver': ['svd', 'lsqr', 'eigen']}
+        knn = LinearDiscriminantAnalysis()
+        clf = grid_search.GridSearchCV(knn, parameters)
+        train_data = get_data('../data/train.csv')
+        train_data = train_data[train_data['AnimalType'] == self.animal_type]
+        train_data = train_data.drop(['AnimalType'], axis=1)
+        train_data = preprocess_data(train_data, self.animal_type)
+        train_data = train_data.dropna()
+        X = train_data.drop(['OutcomeType'], axis=1)
+        y = train_data['OutcomeType']
+        clf.fit(X, y)
+        print clf.best_params_
+
+if __name__ == '__main__':
+    print 'Cat'
+    predictor = LinearDiscriminantAnalysisPredictor('Cat')
+    predictor.find_best_params()
+    print 'Dog'
+    predictor = LinearDiscriminantAnalysisPredictor('Dog')
+    predictor.find_best_params()

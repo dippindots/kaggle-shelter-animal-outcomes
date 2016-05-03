12447b287e4a95cb0a4ebf6f6365de4d025d5d98
Paul Reiners <paul.reiners@gmail.com>
2016-05-03 16:16:46 -0500

    Adding IsGray feature.

--- src/core/preprocessing/feature_extraction_scaling.py
+++ src/core/preprocessing/feature_extraction_scaling.py
@@ -293,6 +293,10 @@ def extract_color_features(data, animal_type):
         return is_cat_color(actual_color, 'Brown Tabby')
     data['IsBrownTabby'] = data['Color'].apply(is_brown_tabby)
 
+    def is_gray(actual_color):
+        return is_cat_color(actual_color, 'Gray')
+    data['IsGray'] = data['Color'].apply(is_gray)
+
     return data
 
 

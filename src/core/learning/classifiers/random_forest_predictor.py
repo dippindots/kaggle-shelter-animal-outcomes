691c1345406e876a26199a3d36f7573afdf4de7a
Paul Reiners <paul.reiners@gmail.com>
2016-05-03 16:53:14 -0500

    Optimizing hyper-params.

--- src/core/learning/classifiers/random_forest_predictor.py
+++ src/core/learning/classifiers/random_forest_predictor.py
@@ -22,9 +22,9 @@ class RandomForestPredictor(PredictorBase):
     def __init__(self, animal_type):
         self.animal_type = animal_type
         if self.animal_type == "Cat":
-            args = {'n_estimators': 80, 'max_depth': 8}
+            args = {'n_estimators': 320, 'max_depth': 8}
         elif self.animal_type == "Dog":
-            args = {'n_estimators': 160, 'max_depth': 6}
+            args = {'n_estimators': 80, 'max_depth': 6}
         else:
             raise RuntimeError("Incorrect animal type")
 
@@ -41,7 +41,7 @@ class RandomForestPredictor(PredictorBase):
 
     def find_best_params(self):
         parameters = {
-            'n_estimators': [80, 160, 320, 640],
+            'n_estimators': [40, 80, 160, 320],
             'max_depth': range(5, 10)}
         rf = RandomForestClassifier()
         clf = grid_search.GridSearchCV(rf, parameters)
